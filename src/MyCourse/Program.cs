3fca02d203691513caf58817d04cee76821791e8
simonevnn <vaninsimonetv@gmail.com>
2024-07-10 11:26:31 +0200

    Aggiunto ASP .NET Core MVC

--- src/MyCourse/Program.cs
+++ src/MyCourse/Program.cs
@@ -1,8 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMvc(MvcOptions =>
+{
+    MvcOptions.EnableEndpointRouting = false;   // Per rendere possibile app.UseMvc()
+});
+
 var app = builder.Build();
 
-app.UseStaticFiles();   //Per permettere l'utilizzo dei file statici
+app.UseStaticFiles();   // Per permettere l'utilizzo dei file statici
 
-app.MapGet("/", () => "Hello World!");
+/**
+ * MIDDLEWARE DI ROUTING
+ * Mappiamo le richieste secondo il template con nome del controller, azione (metodo) e id dell'elemento
+ * 
+ * Da una richiesta https://localhost:5001/Courses/Detail/5
+ * estrae la Route data:
+ * - controller = Courses
+ * - action = Detail
+ * - id = 5 
+ * 
+ * Il codice scritto qui sotto equivale in realtà ad utilizzare app.UseMvcWithDefaultRoute();
+ */
+app.UseMvc(routeBuilder =>
+{
+    routeBuilder.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");    // Con gli "=" indichiamo il valore di default (in caso di risorsa non trovata), e con il "?" indichiamo che l'id è facoltativo
+});
 
 app.Run();
