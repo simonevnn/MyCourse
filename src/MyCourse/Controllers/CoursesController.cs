fe201ebb13bcb185798fb88f4051d99bb55c3476
simonevnn <vaninsimonetv@gmail.com>
2024-07-13 15:17:38 +0200

    Creata View dettaglio corsi

--- src/MyCourse/Controllers/CoursesController.cs
+++ src/MyCourse/Controllers/CoursesController.cs
@@ -10,14 +10,18 @@ namespace MyCourse.Controllers
         {
             var courseService = new CourseService();    // Possiamo scrivere direttamente var per evitare di scrivere sempre il tipo
 
-            List<CourseViewModel> courses = courseService.GetServices();    // Recuperiamo la lista dei corsi dal Service
+            List<CourseViewModel> courses = courseService.GetCourses();    // Recuperiamo la lista dei corsi dal Service
                 
             return View(courses);  // View() prende la view in automatico dal file system, ma è anche possibile specificare un'altra view ad esempio con View("Detail")
         }
 
-        public IActionResult Detail(string id) 
+        public IActionResult Detail(int id) 
         {
-            return View();
+            var courseService = new CourseService();
+
+            CourseDetailViewModel viewModel = courseService.GetCourse(id);
+
+            return View(viewModel);
         }
 
         public IActionResult Search(string title)
--- src/MyCourse/Models/Application/CourseService.cs
+++ src/MyCourse/Models/Application/CourseService.cs
@@ -6,7 +6,7 @@ namespace MyCourse.Models.Application
 {
     public class CourseService
     {
-        public List<CourseViewModel> GetServices()
+        public List<CourseViewModel> GetCourses()
         {
             var courseList = new List<CourseViewModel>();
             var rand = new Random();
@@ -22,13 +22,44 @@ namespace MyCourse.Models.Application
                     FullPrice = new Money(Currency.EUR, rand.NextDouble() > 0.5 ? price : price - 1),
                     Author = "Nome cognome",
                     Rating  = rand.NextDouble()*5.0,
-                    ImagePath = "logo.svg"
+                    ImagePath = "~/logo.svg"
                 };
             
                 courseList.Add(course);
             }
 
             return courseList;
-        }        
+        }
+        
+        public CourseDetailViewModel GetCourse(int id)
+        {
+            var rand = new Random();
+            var price = Convert.ToDecimal(rand.NextDouble() * 10 + 10);
+
+            var course = new CourseDetailViewModel
+            {
+                Id = id,
+                Title = $"Corso {id}",
+                CurrentPrice = new Money(Currency.EUR, price),
+                FullPrice = new Money(Currency.EUR, rand.NextDouble() > 0.5 ? price : price - 1),
+                Author = "Nome cognome",
+                Rating = rand.NextDouble() * 5.0,
+                ImagePath = "~/logo.svg",
+                Description = $"Descrizione {id}",
+                Lessons = new List<LessonViewModel>()
+            };
+
+            for(var i = 1; i <= 5; i++)
+            {
+                var lesson = new LessonViewModel
+                {
+                    Title = $"Lezione {i}",
+                    Duration = TimeSpan.FromSeconds(rand.Next(40, 90))
+                };
+                course.Lessons.Add(lesson);
+            }
+
+            return course;
+        }
     }
 }
